0ecabb3fe2cd3da2daa1743844f1fff737c02bda
Opadonu Seyi <46262267+seyicoded@users.noreply.github.com>
2021-09-09 13:12:23 +0100

    a terminal application to exploit github contri

--- console_run.php
+++ console_run.php
@@ -1,9 +1,9 @@
 <?php
     while(true){
         $myfile = fopen("testrun", "w");
-        $txt = "John Doe\n".sha1(time());
+        $txt = "Git\n".sha1(time());
         fwrite($myfile, $txt);
-        $txt = "John Doe\n".sha1(time());
+        $txt = "Hub\n".sha1(time());
         fwrite($myfile, $txt);
         fclose($myfile);
         exec("git add .");
